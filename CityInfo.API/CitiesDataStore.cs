bbde5f8ce96cdfff7fc8463e04bd1d9faf1c2cd5
suDaCodes <su.le@outlook.com>
2022-10-01 00:04:57 +0700

    add CityDto

---
+++ CityInfo.API/CitiesDataStore.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.API
+{
+    public class CitiesDataStore
+    {
+        public List<CityDto> Cities { get; set; }
+    }
+}
