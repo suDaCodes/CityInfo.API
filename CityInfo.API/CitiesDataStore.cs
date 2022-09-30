1503ee5c74c0b187dac884d1a6c3697f8fa7f1d5
suDaCodes <su.le@outlook.com>
2022-10-01 00:54:10 +0700

    add CitiesDataStore

--- CityInfo.API/CitiesDataStore.cs
+++ CityInfo.API/CitiesDataStore.cs
@@ -1,12 +1,23 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using CityInfo.API.Models;
 
 namespace CityInfo.API
 {
     public class CitiesDataStore
     {
         public List<CityDto> Cities { get; set; }
+
+        // It's a static property, this's a singleton patern network
+        // 
+        public static CitiesDataStore Current { get; } = new CitiesDataStore();
+
+        public CitiesDataStore()
+        {
+            Cities = new List<CityDto>()
+            {
+                new CityDto() { Id = 1, Name = "Ho Chi Minh City", Description = "It's a crowded place, but fun!" },
+                new CityDto() { Id = 2, Name = "Quy Nhon City", Description = "I love this city so much, it's my hometown." },
+                new CityDto() { Id = 3, Name = "Phu Quoc City", Description = "This island is a bright gem!" }
+            };
+        }
     }
 }
